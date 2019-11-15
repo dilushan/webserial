8ad254ec8b952db9da5760ce977bf5f19f4e91df
Dilushan <ddilushan@gmail.com>
2019-11-15 11:09:54 +0530

    Create lcd.js

---
+++ lcd.js
@@ -0,0 +1,20 @@
+	// Start function
+const start = async function() {
+	const requestOptions = {
+      // Filter on devices with the Arduino USB vendor ID.
+      //filters: [{ vendorId: 0x2341 }],
+    };
+
+    // Request an COM port from the user.
+    port = await navigator.serial.requestPort(requestOptions);
+	
+	// Open port.
+    await port.open({
+      baudrate: 2400
+    });
+	
+	// Writing to port.
+	const encoder = new TextEncoder();
+	const writer = port.writable.getWriter();
+	writer.write(encoder.encode(decodeURI('%1B%51%41%31%2E%31%2E%31%2E%39%20%20%20%20%20%20%0D')));
+}
